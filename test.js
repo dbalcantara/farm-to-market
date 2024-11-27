6fdc8fdb9296047aec497cc418d4c04434a30b9c
Paolo Rumbines <pcrumbines@up.edu.ph>
2024-11-28 01:46:26 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,15 +1,7 @@
 import needle from 'needle';
 import './server.js';  // Assuming server.js is your main server file
 
-/*
-    1. Add a test for POST /add-book
-        - object with complete keys and non-empty values
-        - object with 1 missing key
-        - object with complete keys but 1 key has an empty string as its value
-*/
-
-// object with complete keys and non-empty values
-
+// SAMPLE USER
 const user1 = {
     firstName: 'Harry ',
     lastName: 'Potter',
@@ -18,14 +10,7 @@ const user1 = {
     userType: 'customer'
 };
 
-needle.post('http://localhost:27017/newUser', user1, { json: true }, (err, res) => {
-    if (err) throw err;
-    console.log('complete', res.body);  // Should succeed
-});
-
-
-// object with 1 missing key
-
+// SAMPLE ITEM
 const item1 = {
     name: 'cat',
     description: 'furry',
@@ -33,70 +18,35 @@ const item1 = {
     price: 100000,
     quantity: 1
 };
-needle.post('http://localhost:27017/createProduct', item1, { json: true }, (err, res) => {
-    if (err) throw err;
-    console.log('missing key', res.body);  // Should fail with a 400 error
-});
 
-// object with complete keys but 1 key has an empty string as its value
-
+// SAMPLE ORDER
 const order1 = {
     productID: 'Product',
     orderQuantity: 1,
     orderStatus: 1,
     email: 'hp@gmail.com'
 };
-needle.post('http://localhost:27017/createOrder', order1, { json: true }, (err, res) => {
+
+
+needle.post('http://localhost:3000/register', user1, { json: true }, (err, res) => {
+    if (err) throw err;
+    console.log('complete', res.body);  // Should succeed
+});
+
+
+needle.post('http://localhost:3000/register', item1, { json: true }, (err, res) => {
+    if (err) throw err;
+    console.log('missing key', res.body);  // Should fail with a 400 error
+});
+
+
+needle.post('http://localhost:3000/orders', order1, { json: true }, (err, res) => {
     if (err) throw err;
     console.log('empty string', res.body);  // Should fail with a 400 error
 });
 
-/*
-    2. Add a test for GET /find-by-isbn-author
-        - isbn is not existing
-        - author is not existing
-        - isbn and author is not existing
-        - isbn and author is found
-*/
 
-// isbn is not existing
-needle.get('http://localhost:27017/getOrders?email=hp@gmail.com', (err, res) => {
+needle.get('http://localhost:3000/orders?email=hp@gmail.com', (err, res) => {
     if (err) throw err;
     console.log('incomplete details', res.body);  // Should return 404
 });
-
-// // author is not existing
-// needle.get('http://localhost:3000/find-by-isbn-author?isbn=978-0-7475-3269-9&author=J.K+Bowling', (err, res) => {
-//     if (err) throw err;
-//     console.log('author not existing:', res.body);  // Should return 404
-// });
-
-// // isbn and author is not existing
-// needle.get('http://localhost:3000/find-by-isbn-author?isbn=999-0-7475-3269-9&author=J.K+Bowling', (err, res) => {
-//     if (err) throw err;
-//     console.log('isbn and author not existing:', res.body);  // Should return 404
-// });
-
-// // isbn and author is found
-// needle.get('http://localhost:3000/find-by-isbn-author?isbn=978-0-7475-3269-9&author=J.K+Rowling', (err, res) => {
-//     if (err) throw err;
-//     console.log('isbn and author found:', res.body);  // Should return the book
-// });
-
-// /*
-//     3. Add a test for GET /find-by-author
-//         - author is not found
-//         - author is found
-// */
-
-// // author is not found
-// needle.get('http://localhost:3000/find-by-author?author=Denisse+Alcantara', (err, res) => {
-//     if (err) throw err;
-//     console.log('author not found:', res.body);  // Should return 404
-// });
-
-// // author is found
-// needle.get('http://localhost:3000/find-by-author?author=J.K+Rowling', (err, res) => {
-//     if (err) throw err;
-//     console.log('author found:', res.body);  // Should return the books
-// });
