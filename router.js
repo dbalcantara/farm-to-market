ee0f4dd087ddf41e1038f1c8d361eb6ec2e8e53b
scpepito <scpepito@up.edu.ph>
2024-11-29 23:43:28 +0800

    chore: update

--- router.js
+++ router.js
@@ -1,17 +1,35 @@
-import { newUser, login } from "./controllers/authController.js";
-import { createProduct, getProduct, updateProduct } from "./controllers/productController.js";
-import { createOrder, getOrders } from "./controllers/orderController.js";
+import {
+    addUser, getUserbyEmail, updateUserDetails, deleteUser, showAllUser
+} from "./controllers/authController.js";
 
+import {
+    addNewProduct, getProductByID, updateProductDetails, deleteProduct, showAllProducts
+} from "./controllers/productController.js";
 
-const app = (app) => {
-    app.post('/register', newUser);
-    app.post('/login',  login);
-    app.post('/',  createProduct);
-    app.get('/', getProduct);
-    app.put('/:id', updateProduct);
-    app.post('/', createOrder);
-    app.get('/:email', getOrders);
+import {
+    addOrder, getOrderByTransactionId, updateOrder, deleteOrder, showAllOrderFromAUser, showAllOrders
+} from "./controllers/orderController.js";
+
+const router = (app) => {
+    // POST routes (actual API functionality)
+    app.post('/add-user', addUser);
+    app.post('/get-user-by-email', getUserbyEmail);
+    app.post('/update-user', updateUserDetails);
+    app.post('/delete-user', deleteUser);
+    app.post('/show-all-users', showAllUser);
+
+    app.post('/add-product', addNewProduct);
+    app.post('/get-product-by-id', getProductByID);
+    app.post('/update-product', updateProductDetails);
+    app.post('/delete-product', deleteProduct);
+    app.post('/show-all-products', showAllProducts); 
     
-}
-    
-export default app;
+    app.post('/add-order', addOrder);
+    app.post('/get-order-by-transaction-id', getOrderByTransactionId);
+    app.post('/update-order', updateOrder);
+    app.post('/delete-order', deleteOrder);
+    app.post('/show-orders-of-user', showAllOrderFromAUser);
+    app.post('/show-all-orders', showAllOrders);
+};
+
+export default router;
