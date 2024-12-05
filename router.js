bb552e6bff7d12bfacc2ae4b2271b4828e1b5433
unknown <eegarcia4@up.edu.ph>
2024-12-06 05:45:04 +0800

    Pulled router.js from origin/main

--- router.js
+++ router.js
@@ -1,21 +1,37 @@
-import { newUser, login } from "./controllers/authController.js";
-import { createProduct, getProduct, updateProduct } from "./controllers/productController.js";
-import { createOrder, getOrders } from "./controllers/orderController.js";
+import {addUser, getUserbyEmail, updateUserDetails, deleteUser, showAllUser} from "./controllers/authController.js";
+import {addNewProduct, getProductByID, updateProductDetails, deleteProduct, showAllProducts} from "./controllers/productController.js";
+import {addOrder, getOrderByTransactionId, updateOrder, deleteOrder, showAllOrderFromAUser, showAllOrders} from "./controllers/orderController.js";
+import {signup, login, logout, addToCart, removeFromCart, checkout, cancelOrder} from "./actions/customer.js";
 
+const router = (app) => {
+    // POST routes (actual API functionality)
+    app.post('/add-user', addUser);
+    app.post('/get-user-by-email', getUserbyEmail);
+    app.post('/update-user', updateUserDetails);
+    app.post('/delete-user', deleteUser);
+    app.post('/show-all-users', showAllUser);
 
-const app = (app) => {
-    // authentication
-    app.post('/register', newUser);
-    app.post('/login',  login);
-
-    // products
-    app.post('/products', createProduct);
-    app.get('/products', getProduct);
-    app.put('/products/:id', updateProduct);
-
-    // orders
-    app.post('/orders', createOrder);
-    app.get('/orders', getOrders);
-}
+    app.post('/add-product', addNewProduct);
+    app.post('/get-product-by-id', getProductByID);
+    app.post('/update-product', updateProductDetails);
+    app.post('/delete-product', deleteProduct);
+    app.post('/show-all-products', showAllProducts); 
     
-export default app;
+    app.post('/add-order', addOrder);
+    app.post('/get-order-by-transaction-id', getOrderByTransactionId);
+    app.post('/update-order', updateOrder);
+    app.post('/delete-order', deleteOrder);
+    app.post('/show-orders-of-user', showAllOrderFromAUser);
+    app.post('/show-all-orders', showAllOrders);
+
+    app.post('/signup', signup);
+    app.post('/login', login);
+    app.post('/logout', logout);
+
+    app.post('/add-to-cart', addToCart);
+    app.post('/remove-from-cart', removeFromCart);
+    app.post('/checkout', checkout);
+    app.post('/cancel-order', cancelOrder);
+};
+
+export default router;
