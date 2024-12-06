29dfbc382a0851c4144af6b1a874626285b6f77d
unknown <eegarcia4@up.edu.ph>
2024-12-06 11:39:13 +0800

    feat: link login page

--- farm-to-table/src/components/SignUpPage.jsx
+++ farm-to-table/src/components/SignUpPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './SignUpPage.css';
 
 const SignUpPage = () => {
@@ -6,11 +7,16 @@ const SignUpPage = () => {
     <div className="signup-page">
       <h1>Sign Up</h1>
       <form>
-        <input type="text" placeholder="Full Name" required />
+        <input type="text" placeholder="First Name" required />
+        <input type="text" placeholder="Last Name" required />
         <input type="email" placeholder="Email" required />
         <input type="password" placeholder="Password" required />
+        <input type="password" placeholder="Confirm Password" required />
         <button type="submit" className="btn sign-up">Sign Up</button>
       </form>
+      <p className="login-link">
+        Already registered? <Link to="/login">Log in</Link>
+      </p>
     </div>
   );
 };
